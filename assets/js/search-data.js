b5bbccec07a383da4408d86e717aa90494e56864
AcaiLab <yunyin@cs.stonybrook.edu>
2026-01-04 01:34:01 +0000

    Deploying to gh-pages from @ AcaiLab/AcaiLab.github.io@227bfc66e90b0594b007ca3b072705311641ba40 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -71,8 +71,8 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=kmLgZRQAAAAJ", "_blank");
         },
       },{
-        id: 'social-pi_website',
-        title: 'Pi_website',
+        id: 'social-About the PI',
+        title: 'About the pi',
         section: 'Socials',
         handler: () => {
           window.open("https://yinsight.github.io/", "_blank");
