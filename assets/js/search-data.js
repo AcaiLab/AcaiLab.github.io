99636d9a6eca3366e7128e1e442adbadbcbe61f9
AcaiLab <yunyin@cs.stonybrook.edu>
2026-06-15 23:16:09 +0000

    Deploying to gh-pages from @ AcaiLab/AcaiLab.github.io@6505b6ae365603012b23685136fb044fd3a284c4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -54,6 +54,11 @@ ninja.data = [{
           description: "More details coming soon.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-authorship-project",
+          title: 'Authorship Project',
+          description: "More details coming soon.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{
         id: 'social-email',
         title: 'email',
